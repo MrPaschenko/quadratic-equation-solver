c524c998df11862b466ae6347af25b09c9e32185
mrpaschenko <paschenkod2@gmail.com>
2022-02-08 15:30:01 +0200

    function solveQuadraticEquation()

--- Quadratic Equation Solver/main.swift
+++ Quadratic Equation Solver/main.swift
@@ -20,13 +20,23 @@ if (currentState == "Interactive") {
 
     if a != 0 {
         print("b = ", terminator: "")
-        let b = Int(readLine()!) ?? 0
+        let b = Double(readLine()!) ?? 0
         
         print("c = ", terminator: "")
-        let c = Int(readLine()!) ?? 0
+        let c = Double(readLine()!) ?? 0
         
         print("Equation is: \(a) x^2 + \(b) x + \(c) = 0")
-        
+        let answer = solveQuadraticEquation(a: a, b: b, c: c)
+        if answer.count == 2 {
+            print("There are 2 roots")
+            print("x1 = \(answer[0])")
+            print("x2 = \(answer[1])")
+        } else if answer.count == 1 {
+            print("There is 1 root")
+            print("x = \(answer[0])")
+        } else {
+            print("There are no roots")
+        }
     } else {
         print("Error, a != 0")
     }
@@ -34,4 +44,17 @@ if (currentState == "Interactive") {
     
 } else {
     print("Error (currentState variable is incorrect)")
+}
+
+func solveQuadraticEquation(a: Double, b: Double, c: Double) -> [Double] {
+    let d = b * b - 4 * a * c
+    if d > 0 {
+        let x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a)
+        let x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a)
+        return [x1, x2]
+    } else if d == 0 {
+        let x = (-b) / (2 * a)
+        return [x]
+    }
+    return []
 }
