7e1dcbacd92751ccbb2fefd83400f8357d7a5f90
mrpaschenko <paschenkod2@gmail.com>
2022-02-08 15:14:29 +0200

    use Double, check a

--- Quadratic Equation Solver/main.swift
+++ Quadratic Equation Solver/main.swift
@@ -12,17 +12,24 @@ var currentState = "Interactive"
 print("Welcome to Quadratic Equation Solver!")
 
 if (currentState == "Interactive") {
-    print("Please, choose numbers")
-    print("(ax^2 + bx + c = 0)")
-    
-    print("a = ", terminator: "")
-    let a = Int(readLine()!) ?? 0
+    print("ax^2 + bx + c = 0")
+    print("Please, enter real numbers")
 
-    print("b = ", terminator: "")
-    let b = Int(readLine()!) ?? 0
-    
-    print("c = ", terminator: "")
-    let c = Int(readLine()!) ?? 0
+    print("a = ", terminator: "")
+    let a = Double(readLine()!) ?? 0
+
+    if a != 0 {
+        print("b = ", terminator: "")
+        let b = Int(readLine()!) ?? 0
+        
+        print("c = ", terminator: "")
+        let c = Int(readLine()!) ?? 0
+        
+        print("Equation is: \(a) x^2 + \(b) x + \(c) = 0")
+        
+    } else {
+        print("Error, a != 0")
+    }
 } else if (currentState == "Noninteractive") {
     
 } else {
