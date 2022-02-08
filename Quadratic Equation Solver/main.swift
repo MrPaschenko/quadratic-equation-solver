795fdaa8ece672668ece95c7d30f79823318e141
mrpaschenko <paschenkod2@gmail.com>
2022-02-08 21:06:51 +0200

    solveQuadraticEquation(), noninteractive method

--- Quadratic Equation Solver/main.swift
+++ Quadratic Equation Solver/main.swift
@@ -7,18 +7,33 @@
 
 import Foundation
 
-func solveQuadraticEquation(a: Double, b: Double, c: Double) -> [Double] {
+func solveQuadraticEquation(a: Double, b: Double, c: Double) {
+    var answer: [Double] = []
     let d = b * b - 4 * a * c
     print("Discriminant is \(d)")
     if d > 0 {
         let x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a)
         let x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a)
-        return [x1, x2]
+        answer.append(x1)
+        answer.append(x2)
     } else if d == 0 {
         let x = (-b) / (2 * a)
-        return [x]
+        answer.append(x)
     }
-    return []
+    
+    print("Equation is: (\(a)) x^2 + (\(b)) x + (\(c)) = 0")
+
+    if answer.count == 2 {
+        print("There are 2 roots")
+        print("x1 = \(answer[0])")
+        print("x2 = \(answer[1])")
+    } else if answer.count == 1 {
+        print("There is 1 root")
+        print("x = \(answer[0])")
+    } else {
+        print("There are no roots")
+    }
+
 }
 
 func getInput(variable: String) -> Double {
@@ -36,8 +51,8 @@ func getInput(variable: String) -> Double {
     } while more
 }
 
-var currentState = "Interactive"
-//var currentState = "Noninteractive"
+//var currentState = "Interactive"
+var currentState = "Noninteractive"
 
 print("Welcome to Quadratic Equation Solver!")
 
@@ -49,21 +64,22 @@ if (currentState == "Interactive") {
     let b = getInput(variable: "b")
     let c = getInput(variable: "c")
     
-    print("Equation is: (\(a)) x^2 + (\(b)) x + (\(c)) = 0")
-    let answer = solveQuadraticEquation(a: a, b: b, c: c)
-    if answer.count == 2 {
-        print("There are 2 roots")
-        print("x1 = \(answer[0])")
-        print("x2 = \(answer[1])")
-    } else if answer.count == 1 {
-        print("There is 1 root")
-        print("x = \(answer[0])")
-    } else {
-        print("There are no roots")
-    }
+    solveQuadraticEquation(a: a, b: b, c: c)
     
 } else if (currentState == "Noninteractive") {
+    print("Please, enter path to file")
+    let home = FileManager.default.homeDirectoryForCurrentUser
+    let path = readLine()!
+    let linkToFile = home.appendingPathComponent(path)
     
+    let fileContent = try! String(contentsOf: linkToFile)
+    let array = fileContent.map { String($0) }
+
+    let a = Double(array[0])!
+    let b = Double(array[2])!
+    let c = Double(array[4])!
+
+    solveQuadraticEquation(a: a, b: b, c: c)
 } else {
     print("Error (currentState variable is incorrect)")
 }
